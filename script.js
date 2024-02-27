ea6d6b5cf6d3754755f90ae79e2dbcf19aab95ce
alexandrerosapc <alexandrerosasoares3@gmail.com>
2024-02-27 11:45:12 -0300

    Xandelanches com seleção dos itens

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-function selecionar(escolha){
-   const busca = document.querySelector('.selecionado');
-   if (busca !== null){
-    busca.classList.remove('selecionado')
+function selecionarPrato(escolha1){
+   const busca1 = document.querySelector('.pratos .selecionado');
+
+   if (busca1 !== null){
+    busca1.classList.remove('selecionado');
    }
-   escolha.classList.toggle(".selecionado");
+   escolha1.classList.add('selecionado');
 }
+
+function selecionarBebida(escolha2){
+    const busca2 = document.querySelector('.bebidas .selecionado');
+ 
+    if (busca2 !== null){
+     busca2.classList.remove('selecionado');
+    }
+    escolha2.classList.add('selecionado');
+ }
+ 
+ function selecionarSobremesa(escolha3){
+    const busca3 = document.querySelector('.sobremesas .selecionado');
+ 
+    if (busca3 !== null){
+     busca3.classList.remove('selecionado');
+    }
+    escolha3.classList.add('selecionado');
+ }
